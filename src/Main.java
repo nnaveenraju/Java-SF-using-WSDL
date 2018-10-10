51e0ecf19f214bbec5a54942307a014e001c2875
Naveen - Personal <nnaveenraju@users.noreply.github.com>
2018-10-10 02:28:55 -0400

    create account

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import com.sforce.soap.enterprise.EnterpriseConnection;
 import com.sforce.soap.enterprise.GetUserInfoResult;
+import com.sforce.soap.enterprise.SaveResult;
 import com.sforce.soap.enterprise.sobject.Account;
+import com.sforce.soap.enterprise.sobject.SObject;
 import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
 
@@ -11,7 +13,7 @@ public class Main {
 
 		ConnectorConfig c = new ConnectorConfig();
 		c.setUsername("nnaveenraju@yahoo.com");
-		c.setPassword("************");
+		c.setPassword("*******");
 		
 		c.setAuthEndpoint("https://login.salesforce.com/services/Soap/c/41.0");
 		EnterpriseConnection ec = new EnterpriseConnection(c) ;
@@ -19,7 +21,17 @@ public class Main {
 		GetUserInfoResult r = ec.getUserInfo();
 		System.out.println(r.getUserFullName());
 		
+		Account a = new  Account();
+		a.setName("Java Test");
 		
+		SaveResult[] re = ec.create( new SObject[] { a });
+		
+		for (SaveResult saveResult : re) {
+			if (saveResult.isSuccess())
+			{
+				System.out.println("Created Account successfully");
+			}
+		}
 		 
 	}
 
